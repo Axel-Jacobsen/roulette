bbd753a4ddf4bf67b36f111f7a8988601c3d47cb
Axel Jacobsen <axelnj44@gmail.com>
2023-01-26 19:51:02 -0800

    thoughts

--- roulette.sh
+++ roulette.sh
@@ -7,6 +7,13 @@
 # - whatever TODO, FIXME, or type error you get, you must fix!
 # - total number of TODOs, FIXMEs, or type error must go down
 
+# 'TODO's and thoughts
+#
+# - It would be sweet to be able to add / configure tools (e.g. mypy, whatever else)
+# - Configure FIXME / TODO whatever else?
+# - How to make it cross platform? Where is shuf supported?
+
+
 our_problems=$({ mypy . --no-error-summary; git grep -niE '(FIXME|TODO)'; })
 
 echo "number of 'FIXME's, 'TODO's, and type errors: $(echo "$our_problems" | wc -l)"
