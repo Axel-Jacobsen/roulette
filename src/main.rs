8fe98c9e50d8bda97e0b5882ef683bc220fb411f
Axel Jacobsen <axelnj44@gmail.com>
2023-01-31 21:39:15 -0800

    refactor main

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,29 @@ use termcolor::{ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 mod commands;
 
-fn main() -> std::io::Result<()> {
-    let mut stdout = StandardStream::stdout(ColorChoice::Always);
 
+fn get_output(funcs: &Vec<fn() -> std::io::Result<Vec<String>>>) -> Vec<String> {
     let mut vals: Vec<String> = vec![];
-    let funcs = [commands::git_grep, commands::mypy, commands::ruff];
 
+    // TODO run funcs concurrently?
     for func in funcs {
         match func() {
             Ok(vs) => vals.extend(vs),
-            Err(_) => println!("log io error for git_gstrsrep"),
+            Err(_) => (),
         }
     }
 
-    let mut color_spec = ColorSpec::new();
+    vals
+}
 
-    write!(&mut stdout, "Number of 'pockets': ")?;
+fn main() -> std::io::Result<()> {
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
 
-    color_spec.set_bold(true);
-    stdout.set_color(&color_spec)?;
+    // TODO filter funcs somehow!
+    let funcs: Vec<fn() -> std::io::Result<Vec<String>>> =
+        vec![commands::git_grep, commands::mypy, commands::ruff];
+
+    let vals = get_output(&funcs);
 
     let total_len = vals.len();
     if total_len == 0 {
@@ -35,13 +39,22 @@ fn main() -> std::io::Result<()> {
         return Ok(());
     }
 
+    let mut color_spec = ColorSpec::new();
+
+    write!(&mut stdout, "Number of 'pockets': ")?;
+
+    color_spec.set_bold(true);
+    stdout.set_color(&color_spec)?;
+
     writeln!(&mut stdout, "{}", total_len)?;
 
     color_spec.clear();
     stdout.set_color(&color_spec)?;
 
     let lucky_number = rand::thread_rng().gen_range(0..total_len);
+
     let line = &vals[lucky_number];
+
     writeln!(&mut stdout, "{}", line)?;
 
     Ok(())
